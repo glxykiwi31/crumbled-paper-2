1209d6baf1739257e83307e12c4e3e9c33786c22
glxykiwi31 <63457068+glxykiwi31@users.noreply.github.com>
2020-06-24 12:28:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1;
+
+function setup(){
+    var canvas = createCanvas(1600,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    paper = new Paper(200,200);
+    ground = new Ground(800,height,1600,20);
+    dustbin = new Dustbin(1100,380,200,20);
+}
+function draw(){
+    background("white");
+    Engine.update(engine);
+    text("x:y "+mouseX+" "+mouseY,mouseX,mouseY);
+    
+    ground.display();
+    paper.display();
+    dustbin.display();
+}
+
+function keyPressed(){
+    if(keyCode === UP_ARROW){
+    console.log("kaveesh")
+Matter.Body.applyForce(paper.body,paper.body.position,{x:85,y:-85});
+
+    }
+}
